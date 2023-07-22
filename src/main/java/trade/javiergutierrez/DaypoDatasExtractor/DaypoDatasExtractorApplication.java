30006e9131f50218b17c402d948efce48de6be9c
Guti <gutifer666@gmail.com>
2023-07-22 23:19:11 +0200

    first commit

---
+++ src/main/java/trade/javiergutierrez/DaypoDatasExtractor/DaypoDatasExtractorApplication.java
@@ -0,0 +1,13 @@
+package trade.javiergutierrez.DaypoDatasExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaypoDatasExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaypoDatasExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trade/javiergutierrez/DaypoDatasExtractor/DaypoDatasExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package trade.javiergutierrez.DaypoDatasExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaypoDatasExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
