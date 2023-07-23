95695000f6a2150f5606794e4412ed7aab10ced2
Guti <gutifer666@gmail.com>
2023-07-23 20:49:10 +0200

    added upload file

--- src/main/java/trade/javiergutierrez/DaypoDatasExtractor/controllers/SendXMLController.java
+++ src/main/java/trade/javiergutierrez/DaypoDatasExtractor/controllers/SendXMLController.java
@@ -1,15 +1,24 @@
 package trade.javiergutierrez.DaypoDatasExtractor.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 // Leer un fichero XML enviado desde Postman
 @RestController
 public class SendXMLController {
-    String xmlFile = "<c><t>0</t><p>El lenguaje SQL:</p><c>2111</c><r><o>Es estándar.</o><o>Es propietario, pertenece a Oracle.</o><o>Procede del lenguaje Pascal.</o><o>Ninguna de las respuestas anteriores es correcta.</o></r></c>";
     @PostMapping ("/sendxml")
     public String sendXMLFile(@RequestBody String xmlFile) {
         System.out.println(xmlFile);
         return xmlFile;
     }
+
+    @PostMapping ("/uploadfile")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        return file.getOriginalFilename();
+    }
+
+
+
 
 }
